30398e6d21a40a175ee6e19f762ed575b4dcf879
Serj777rus <133807735+Serj777rus@users.noreply.github.com>
2024-07-02 16:07:34 +0300

    x

--- src/components/Concerts.vue
+++ src/components/Concerts.vue
@@ -9,7 +9,7 @@
             <p>{{ partys.month }}</p>
             <p>{{ partys.date }}</p>
             <p>{{ partys.name }}</p>
-            <iframe :src="partys.video"></iframe>
+            <video :src="partys.video" controls muted></video>
             <div class="party_photo">
                 <img v-for="photo in partys.photo" :key="photo" :src="photo">
             </div>
@@ -35,8 +35,8 @@ import HeadMenu from './UI_components/HeadMenu.vue';
                     date: '14',
                     name: 'Отчетный концерт',
                     place: 'Зал "Радуга"',
-                    video: 'https://youtu.be/G6DFj6m6jHA?si=m5OUSH27NfyObWND',
-                    photo: ['https://www.google.com/url?sa=i&url=https%3A%2F%2Fmimigram.ru%2Fblog%2Fchto-takoe-foto-tekhnologiya-ili-iskusstvo%2F&psig=AOvVaw2POCYeVX-hwWbYmMJsmLb-&ust=1719948458561000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCX3MXJhocDFQAAAAAdAAAAABAE', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canva.com%2Fru_ru%2Fobuchenie%2Ffotografii-prirody-50-sovetov%2F&psig=AOvVaw2POCYeVX-hwWbYmMJsmLb-&ust=1719948458561000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCX3MXJhocDFQAAAAAdAAAAABAI', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Flifehacker.ru%2Fspecial%2Ffujifilm%2Fugaday-chto-na-foto%2F&psig=AOvVaw2POCYeVX-hwWbYmMJsmLb-&ust=1719948458561000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCX3MXJhocDFQAAAAAdAAAAABAS']
+                    video: 'https://videocdn.cdnpk.net/videos/526eb1e2-9ac0-4021-bd1a-4d593fe8bda6/horizontal/previews/clear/large.mp4?token=exp=1719918599~hmac=6bcfade5d37ead7842c678d226de7d548dfe21005fbb783d870ef4f4bd04c385',
+                    photo: ['https://img.freepik.com/free-vector/realistic-russia-day-event_23-2148559618.jpg?t=st=1719918207~exp=1719921807~hmac=db07495bba9b15295418a3a5e200216e065de5bb3b6f1121fb0e9ff73b6cea32&w=1480', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Flifehacker.ru%2Fspecial%2Ffujifilm%2Fugaday-chto-na-foto%2F&psig=AOvVaw2POCYeVX-hwWbYmMJsmLb-&ust=1719948458561000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNCX3MXJhocDFQAAAAAdAAAAABAS']
                 },
                 {
                     id: 1,
